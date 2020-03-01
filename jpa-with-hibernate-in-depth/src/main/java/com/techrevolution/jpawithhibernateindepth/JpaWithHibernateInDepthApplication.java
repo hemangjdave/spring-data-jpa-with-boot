7aa24987ee0534d960ae9b736ac5cb0119fbf821
Hemang Dave <hemangjdave@gmail.com>
2020-03-02 01:39:03 +0530

    Spring jdbc demo

---
+++ jpa-with-hibernate-in-depth/src/main/java/com/techrevolution/jpawithhibernateindepth/JpaWithHibernateInDepthApplication.java
@@ -0,0 +1,41 @@
+package com.techrevolution.jpawithhibernateindepth;
+
+import com.techrevolution.jpawithhibernateindepth.entity.Course;
+import com.techrevolution.jpawithhibernateindepth.repository.CourseRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class JpaWithHibernateInDepthApplication implements CommandLineRunner {
+
+	private final CourseRepository courseRepository;
+
+	@Autowired
+	public JpaWithHibernateInDepthApplication(CourseRepository courseRepository) {
+		this.courseRepository = courseRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWithHibernateInDepthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Course with 10001 id is:--{}", courseRepository.findById(10001));
+		log.info("Inserting new course:--{}" ,
+				courseRepository.saveOrUpdateCourse(
+						new Course("Effective java")
+				)
+		);
+
+		log.info("Updating course:--{}" , courseRepository.saveOrUpdateCourse(
+				new Course(10002L ,"Spring boot in depth in 28Minutes")
+		));
+
+//		log.info("Deleting course with id:--10001 and status is:--{}", courseRepository.deleteByid(10001));
+	}
+}
---
+++ jpa-with-hibernate-in-depth/src/test/java/com/techrevolution/jpawithhibernateindepth/JpaWithHibernateInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techrevolution.jpawithhibernateindepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithHibernateInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
